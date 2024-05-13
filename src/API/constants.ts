be57b5094aa7090908a45f197fda8b57d7f0f535
starBURST <sidorenko.1992god@gmail.com>
2024-05-14 00:05:55 +0500

    added openapi contract

---
+++ src/API/constants.ts
@@ -0,0 +1,5 @@
+export const OUR_API_ADDRESS = 'http://127.0.0.1:8888';
+export enum OUR_API_ENDPOINTS {
+    register = 'register',
+    auth = 'auth'
+}
--- src/Router.tsx
+++ src/Router.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from './App';
 import Auth from './pages/Auth/Auth';
